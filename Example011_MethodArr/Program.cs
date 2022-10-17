2649d59ad2b47c711897730d2f5a9d0be78fa57e
NataliyaYadykina <nataliya.yadykina@yandex.ru>
2022-10-17 15:33:59 +0300

    Add Example011 Method Array

---
+++ Example011_MethodArr/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {13,24,3,14,58,45,6,47,58};
+
+int n = array.Length;
+int find = 58;
+
+int i = 0;
+
+while(i < n)
+{
+    if(array[i] == find)
+    {
+        Console.WriteLine(i);
+        break;
+    }
+    i++;
+}
